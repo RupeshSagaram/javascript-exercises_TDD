4a8400ecb5da39f3ef2d6f829fc98015753ad5bb
Rupesh Sagaram <s.v.rupesh97@gmail.com>
2024-07-27 18:52:53 +0530

    completed js exercises 1&2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
+const repeatString = function(aString,number) {
 
+    if(number===0){
+        return '';
+     } else if(number<0){
+        return 'ERROR';
+     } else if (aString===''){
+        return '';
+     }
+ 
+   stringName = aString; 
+result = aString.repeat(number);
+
+ return result;
+  
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
