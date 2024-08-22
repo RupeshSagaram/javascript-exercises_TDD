5ead4f4bed1c004a5d5b769358bbc6c52d0e0fb4
Rupesh Sagaram <s.v.rupesh97@gmail.com>
2024-08-22 23:55:54 +0530

    finished 08 calc and 04 is still incomplete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  arrContainer = arr;
+  let sum = arrContainer.reduce((total,current) =>{
+    return total + current;
+  },0);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  arrContainer = arr;
+    let multiply = arrContainer.reduce((total,current)=>{
+        return total * current;
+    },1);
+    return multiply;
 };
 
-const power = function() {
-	
+const power = function(num,power) {
+	return Math.pow(num,power);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) { 
+        if(num===0 || num ===1){
+          return 1;
+        } else {
+          return num * factorial(num-1);
+        }
 };
 
 // Do not edit below this line
