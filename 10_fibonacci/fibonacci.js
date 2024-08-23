02761100eb4a87b1ba0bf67fd8ed6c05cc671295
Rupesh Sagaram <s.v.rupesh97@gmail.com>
2024-08-23 21:24:36 +0530

    completed fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
+    let fib = [1,1];
+    if(n ==0){
+        return 0;
+    } else if(n<0){
+        return 'OOPS';
+    }
+    else{
+    for(let i= 2; i<=n; i++){ 
+    fib.push(fib[i-1]+fib[i-2]);
+    };
+    return fib[n-1];
+}
 };
 
 // Do not edit below this line
