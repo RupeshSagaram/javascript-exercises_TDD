6076776af2bebe371fc5a72fb1c017ce3bfb25f9
Rupesh Sagaram <s.v.rupesh97@gmail.com>
2024-08-23 16:46:08 +0530

    finished palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let re = /[\W_]/g;
+ let str = word.toLowerCase().replace(re,'');
+ let reverseStr = str.split('').reverse().join('');
+ if(str ===reverseStr){
+    return true;
+ } else{ 
+    return false;
+    
+ }
 };
 
 // Do not edit below this line
