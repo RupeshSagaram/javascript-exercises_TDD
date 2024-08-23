85ab6b2d699e11d19585b1811e63359ab74eb1fe
Rupesh Sagaram <s.v.rupesh97@gmail.com>
2024-08-23 21:46:42 +0530

    finished 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+let newArr = [];
+const getTheTitles = function(arr) {
+    for(i=0; i<arr.length; i++){
+         newArr.push(arr[i].title);  
+    }
+    return newArr;
+    
 };
 
 // Do not edit below this line
