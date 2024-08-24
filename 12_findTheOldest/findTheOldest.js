4d872538fcd1cc08f75db1674fee061212b14e9f
Rupesh Sagaram <s.v.rupesh97@gmail.com>
2024-08-24 12:08:42 +0530

    completed 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
 
+    currentYear = new Date().getFullYear();
+    let oldestPerson = arr.reduce((older, current) =>{
+
+        let prevPerAge = (typeof older.yearOfDeath === 'undefined' ? currentYear : older.yearOfDeath) - older.yearOfBirth;
+        let curPerAge = (typeof current.yearOfDeath === 'undefined' ? currentYear : current.yearOfDeath) - current.yearOfBirth;
+
+        return prevPerAge > curPerAge ? older : current;
+    
+             
+},0);
+return oldestPerson;
 };
 
 // Do not edit below this line
